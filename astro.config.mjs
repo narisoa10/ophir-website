e0f8f82d6785fbcdf8979b166a16d21e63d992f2
Naris <narisoa10@gmail.com>
2026-07-28 16:15:40 -0400

    feat(website): add sitemap and robots configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { siteConfig } from "./src/config/site.ts";
 
 export default defineConfig({
   site: siteConfig.siteUrl,
+
+  integrations: [
+    sitemap({
+      filter: (page) => page !== new URL("/", siteConfig.siteUrl).toString(),
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
